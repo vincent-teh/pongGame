36c2704c50a6fe09ede0b6fbb70f6bba12ea3805
vincent <yusheng.teh13@gmail.com>
2022-10-29 18:44:10 +0800

    main.lua: successfully printf hello world

--- main.lua
+++ main.lua
@@ -3,17 +3,17 @@ WINDOW_HEIGHT = 720
 
 function love.load()
         love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
-                full_screen = false,
-                reizeable = false,
-                vsync = true
+                fullscreen = false,
+                resizable = false,
+                vsync = true            -- vertical sync
         })
 end
 
 function love.draw()
         love.graphics.printf(
-                "Hello World",
-                0,
-                WINDOW_WIDTH / 2 - 15,
-                30,
-                'center')
+                "Hello World",          -- string
+                0,                      -- start value of x (0 indicates left most)
+                WINDOW_HEIGHT / 2 - 6,  -- start value of y
+                WINDOW_WIDTH,           -- number of width to center things
+                'center')               -- alignment mode
 end
