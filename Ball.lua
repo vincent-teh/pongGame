ac7a41d153916746d3b7e0474328de10ba92c0da
vincent <yusheng.teh13@gmail.com>
2022-11-05 22:41:03 +0800

    change indentation to tab

--- Ball.lua
+++ Ball.lua
@@ -3,24 +3,24 @@ Ball = class()
 BALL_XSPPED = 150
 
 function Ball:init(width, height)
-        self.x = VIRTUAL_WIDTH / 2 - BALL_SIZE / 2
-        self.y = VIRTUAL_HEIGHT / 2 - BALL_SIZE / 2
-        self.width = width
-        self.height = height
+	self.x = VIRTUAL_WIDTH / 2 - BALL_SIZE / 2
+	self.y = VIRTUAL_HEIGHT / 2 - BALL_SIZE / 2
+	self.width = width
+	self.height = height
 
-        self.dx = math.random(2) == 1 and BALL_XSPPED or -BALL_XSPPED
-        self.dy = math.random(-50, 50)
+	self.dx = math.random(2) == 1 and BALL_XSPPED or -BALL_XSPPED
+	self.dy = math.random(-50, 50)
 end
 
 function Ball:reset()
-        self.x = VIRTUAL_WIDTH / 2 - BALL_SIZE / 2
-        self.y = VIRTUAL_HEIGHT / 2 - BALL_SIZE / 2
-        self.dx = math.random(2) == 1 and BALL_XSPPED or -BALL_XSPPED
-        self.dy = math.random(-50, 50)
+	self.x = VIRTUAL_WIDTH / 2 - BALL_SIZE / 2
+	self.y = VIRTUAL_HEIGHT / 2 - BALL_SIZE / 2
+	self.dx = math.random(2) == 1 and BALL_XSPPED or -BALL_XSPPED
+	self.dy = math.random(-50, 50)
 end
 
 function Ball:draw()
-        love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+	love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
 end
 
 function Ball:update(dt)
@@ -29,14 +29,14 @@ function Ball:update(dt)
 end
 
 function Ball:collide(Paddle)
-        if self.x > Paddle.x + 3 or Paddle.x > self.x + 4 then
-                -- no collision occur
-                return false
-        end
-        if self.y > Paddle.y + PADDLE_HEIGHT or self.y + 4 < Paddle.y then
-                -- need to add marks to next player
-                return false
-        end
-        -- collision occur, need to update the ball movement
-        return true
+	if self.x > Paddle.x + 3 or Paddle.x > self.x + 4 then
+		-- no collision occur
+		return false
+	end
+	if self.y > Paddle.y + PADDLE_HEIGHT or self.y + 4 < Paddle.y then
+		-- need to add marks to next player
+		return false
+	end
+	-- collision occur, need to update the ball movement
+	return true
 end
--- Paddle.lua
+++ Paddle.lua
@@ -3,20 +3,20 @@ PADDLE_SPEED = 200
 PADDLE_HEIGHT = 50
 
 function Paddle:init(x, y)
-        self.x = x
-        self.y = y
-        self.score = 0
+	self.x = x
+	self.y = y
+	self.score = 0
 end
 
 function Paddle:render()
-        love.graphics.rectangle('fill', self.x, self.y, 3, PADDLE_HEIGHT)	-- rectangle on the left
+	love.graphics.rectangle('fill', self.x, self.y, 3, PADDLE_HEIGHT)	-- rectangle on the left
 end
 
 function Paddle:update(dt, upKey, downKey)
-        if love.keyboard.isDown(upKey) then
-                self.y = math.max(0, self.y - PADDLE_SPEED * dt)
-        end
-        if love.keyboard.isDown(downKey) then
-                self.y = math.min(VIRTUAL_HEIGHT - 50, self.y + PADDLE_SPEED * dt)
-        end
+	if love.keyboard.isDown(upKey) then
+		self.y = math.max(0, self.y - PADDLE_SPEED * dt)
+	end
+	if love.keyboard.isDown(downKey) then
+		self.y = math.min(VIRTUAL_HEIGHT - 50, self.y + PADDLE_SPEED * dt)
+	end
 end
